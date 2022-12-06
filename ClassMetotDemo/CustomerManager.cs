e910e354b4334b0816014e5f1b7c8daf6313e2b7
HakkaHalmiev <HakkaHalmiev@DESKTOP-KT8AQSM>
2022-12-06 00:30:54 +0000

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Add (Customer customerNew)
+        {
+
+            Console.WriteLine("Customer Added : "+customerNew.CustomerName);
+
+        }
+        public void Delete(Customer customerDelete)
+        {
+
+            Console.WriteLine("Customer Deleted : "+customerDelete.CustomerName);
+        }
+        public void List(Customer customer)
+        {
+            Console.WriteLine(customer.CustomerId);
+            Console.WriteLine(customer.CustomerName);
+            Console.WriteLine(customer.CustomerOrderName);
+        }
+
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+
+            Customer customer1 = new Customer();
+            Customer customer2 = new Customer();
+
+            customer1.CustomerId = 1;
+            customer1.CustomerName = "John";
+            customer1.CustomerOrderName = "Iphone 5 , Kindle , Story-Book ";
+
+            
+            customer2.CustomerId = 2;
+            customer2.CustomerName = "Selin";
+            customer2.CustomerOrderName = "Iphone 14 , Strawberry , Sci-Fi Books";
+
+
+            CustomerManager customerManager = new CustomerManager();
+            Customer[] customers = new Customer[] { customer1, customer2 };
+
+           
+
+            Console.WriteLine("----------Options--------------");
+            Console.WriteLine("1 : Add");
+            Console.WriteLine("2 : Delete");
+            Console.WriteLine("3 : List");
+            Console.WriteLine("-------------------------------");
+
+            int number;
+            number = Convert.ToInt32(Console.ReadLine());
+
+            if (number == 1)
+            {
+                customerManager.Add(customer1);
+                customerManager.Add(customer2);
+            }
+            else if (number == 2)
+            {
+                customerManager.Delete(customer1);
+                customerManager.Delete(customer2);
+            }
+            else if (number == 3)
+            {
+                foreach (var selected in customers)
+                {
+                    customerManager.List(selected);
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
